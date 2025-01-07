aeff57cfb0cbd1979bb1c36ad289c9faadb065e6
jungse8609 <69056797+jungse8609@users.noreply.github.com>
2025-01-07 14:46:11 +0900

    [B5] BOG 10807. 개수 세기(Updated)

--- cpp/solved/b2_10807_개수세기.cpp
+++ cpp/solved/b2_10807_개수세기.cpp
@@ -4,6 +4,25 @@
 #include <algorithm>
 using namespace std;
 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n, v;
+    int a[202] = {};
+    cin >> n;
+    while (n--)
+    {
+        int t;
+        cin >> t;
+        a[t + 100] += 1;
+    }
+    cin >> v;
+
+    cout << a[v + 100];
+}
+
+/* Sol 1
 int n, v;
 int a[102] = {};
 
@@ -27,3 +46,5 @@ int main()
 
     cout << result;
 }
+*/
+
