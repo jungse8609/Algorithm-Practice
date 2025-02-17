517c206cfe1a518d7eb09e17a5830d17da0fe370
jungse8609 <jungse8609@naver.com>
2025-02-17 21:32:41 +0900

    [S2] BOG 1874 스택수열 SOL2

--- cpp/solved/s2_1874_스택수열.cpp
+++ cpp/solved/s2_1874_스택수열.cpp
@@ -7,46 +7,77 @@ using namespace std;
 
 int main()
 {
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
     ios::sync_with_stdio(0);
 
     int n; cin >> n;
     stack<int> s;
-    queue<int> q;
-    for (int i=1; i<=n; ++i)
-    {
-        int a; cin >> a;
-        q.push(a);
-    }
+    int cnt = 1;
 
     string result = "";
-
-    // 숫자 입력 받아
-    // 만약 s.top()이 입력값이 아니다
-    //      입력값까지 숫자를 추가해야 해
-    for (int i=1; i<=n; ++i)
+    while (n--)
     {
-        s.push(i);
-        result += "+\n";
-
-        // 같으면 계속 빼야지
-        while (s.top() == q.front())
+        int t; cin >> t;
+        while (cnt <= t)
         {
-            s.pop();
-            q.pop();
-            result += "-\n";
-
-            if (s.empty())
-                break;
+            s.push(cnt++);
+            result += "+\n";
         }
-    }
+        if (s.top() != t)
+        {
+            cout << "NO";
+            return 0;
+        }
 
-    if (!s.empty())
-    {
-        cout << "NO";
+        s.pop();
+        result += "-\n";
     }
-    else
-    {
-        cout << result;
-    }
+    cout << result;    
 }
+
+// SOL 1
+// int main()
+// {
+//     cin.tie(0); cout.tie(0);
+//     ios::sync_with_stdio(0);
+
+//     int n; cin >> n;
+//     stack<int> s;
+//     queue<int> q;
+//     for (int i=1; i<=n; ++i)
+//     {
+//         int a; cin >> a;
+//         q.push(a);
+//     }
+
+//     string result = "";
+
+//     // 숫자 입력 받아
+//     // 만약 s.top()이 입력값이 아니다
+//     //      입력값까지 숫자를 추가해야 해
+//     for (int i=1; i<=n; ++i)
+//     {
+//         s.push(i);
+//         result += "+\n";
+
+//         // 같으면 계속 빼야지
+//         while (s.top() == q.front())
+//         {
+//             s.pop();
+//             q.pop();
+//             result += "-\n";
+
+//             if (s.empty())
+//                 break;
+//         }
+//     }
+
+//     if (!s.empty())
+//     {
+//         cout << "NO";
+//     }
+//     else
+//     {
+//         cout << result;
+//     }
+// }
