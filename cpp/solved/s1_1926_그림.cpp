10f9f4519c7408b5cf6a27c77956516d93e0051a
jungse8609 <69056797+jungse8609@users.noreply.github.com>
2025-03-28 16:51:47 +0900

    fix: 코드 업데이트

--- cpp/solved/s1_1926_그림.cpp
+++ cpp/solved/s1_1926_그림.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 using namespace std;
 
-int n, m, result = -1;
+int n, m, cnt = 0, area = 0;
 vector<vector<int>> adj;
 vector<vector<bool>> visited;
 int dx[4] = {1, 0, -1, 0};
@@ -11,11 +11,13 @@ int dy[4] = {0, 1, 0, -1};
 
 void bfs(int x, int y)
 {
-    vector<vector<bool>> discovered(n, vector<bool>(m, false));
     queue<pair<int, int>> q;    
     q.push({x, y});
-    result += 1;
-    discovered[x][y] = true;
+
+    visited[x][y] = true;
+
+    int curArea = 0;
+    curArea += 1;
 
     while (!q.empty())
     {
@@ -25,28 +27,33 @@ void bfs(int x, int y)
         {
             int nx = here.first + dx[i];
             int ny = here.second + dy[i];
-
-            if (!discovered[nx][ny] && adj[nx][ny] == 1)
+            
+            if (0 <= nx && nx < n && 0 <= ny && ny < m)
             {
-                discovered[nx][ny] = true;
-                q.push({nx, ny});
-                result += 1;
+                if (!visited[nx][ny] && adj[nx][ny] == 1)
+                {
+                    visited[nx][ny] = true;
+                    q.push({nx, ny});
+                    curArea += 1;
+                }
             }
         }
     }
 
-    for (int i=0; i<n; ++i)
-        for (int j=0; j<m; ++j)
-            if (discovered[i][j] == 1)
-                visited[i][j] = 1;
+    area = max(area, curArea);
 }
 
 void bfsAll()
 {
+    // N^2
     for (int i=0; i<n; ++i)
         for (int j=0; j<m; ++j)
             if (!visited[i][j] && adj[i][j] == 1)
+            {
+                cnt += 1;
                 bfs(i, j);
+            }
+                
 }
 
 int main()
@@ -54,14 +61,16 @@ int main()
     cin.tie(0); ios::sync_with_stdio(0);
 
     cin >> n >> m;
+
     adj = vector<vector<int>>(n, vector<int>(m));
     visited = vector<vector<bool>>(n, vector<bool>(m, false));
 
+    // N^2
     for (auto& line : adj)
         for (auto& e : line)
             cin >> e;
 
     bfsAll();
 
-    cout << result;
+    cout << cnt << '\n' << area;
 }
